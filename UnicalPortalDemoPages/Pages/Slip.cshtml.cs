2a32f46591c4ee788b0fec937d557800c532a3d3
Kyree henry <ene.henry.eh@gmail.com>
2023-03-09 15:11:49 +0100

    Added Slip Page

---
+++ UnicalPortalDemoPages/Pages/Slip.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnicalPortalDemoPages.Pages
+{
+    public class SlipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
